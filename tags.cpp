18238bf3ca65fcaf37fd18047b58f42eba2c0f73
Peter <pevaspok@gmail.com>
2021-01-23 01:21:23 +0300

    add find query

--- tags.cpp
+++ tags.cpp
@@ -1,41 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <algorithm>
 using namespace std;
 
 string get_tag(string s, int* pos, char sep){
     string tmp;
-    while (s[*pos] != sep) tmp += s[(*pos)++];
+    while (s[(*pos)] != sep) tmp += s[(*pos)++];
     return tmp;
 }
 
-
-
+string find_query(vector <string> n, string q, vector <string> v){
+    for (int p = 0; p < n.size(); p++){
+            if (n[p] == q) return v[p];
+    }
+    return "Not Found!";    
+}
 
 int main(){
-    int n, q, i = 1;
-    string tag, line;
-    vector <string> tags;
-    vector <vector <string>> names, values;
-    names.resize(5);
-    values.resize(5);
-    //cin >> n >> q;
-    // getline(cin, line);
-    line = "<tag1 v1 = \"ad\" v2 = \"gd\">";
-    if (line[i] != '/') {
-        if (tag.size() > 0) tag += '.'; 
-        tag += get_tag(line, &i, ' ');
-        tags.push_back(tag);
-        while (i < (line.size() - 1)){
-            i++;
-            names[0].push_back(get_tag(line, &i, ' '));
-            i += 4;
-            values[0].push_back(get_tag(line, &i, '"'));
-            i++;
+    int n, q, i;
+    string tag, line1, ftag, fname;
+    vector <string> names, values;
+    cin >> n >> q;
+    cin.ignore();
+    for (int j = 0; j < n; j++){ 
+        i = 1;
+        getline(cin, line1);
+        if (line1[i] != '/') {
+            if (tag.size() > 0) tag += '.'; 
+            tag += get_tag(line1, &i, ' ');
+            while (i < (line1.length() - 1)){
+                i++;
+                names.push_back(tag + '~' + get_tag(line1, &i, ' '));
+                i += 4;
+                values.push_back(get_tag(line1, &i, '"'));
+                i++;
+            }
         }
+        else {
+            int pos_point = 0;
+            if (tag.find('.') != -1) pos_point = tag.find('.');
+            tag.erase(pos_point);
+        }    
     }
-    cout << tags[0] << '\n' << values[0][0] << '\n'<< values[0][1];
-    
-
+    for (int j = 0; j < q; j++){
+        getline(cin, line1);
+        cout << find_query(names, line1, values) << '\n'; 
+    }
     return 0;
 }
