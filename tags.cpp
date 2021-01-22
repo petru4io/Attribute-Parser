3cce2d4274e66f9115962e845203f257952025dc
Peter <pevaspok@gmail.com>
2021-01-22 21:46:10 +0300

    added tags function

---
+++ tags.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    string line;
+    getline(cin, line);
+    cout << line << '\n';
+    return 0;
+}
